e5d0292dbc7e76428406269d510ced1f8dbfea5c
andresmarcelo99 <marcelo.andres99.ma@gmail.com>
2020-06-18 15:30:09 -0600

    removed  nodemailer

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,5 +1,4 @@
 const { Pool } = require("pg");
-const nodemailer = require("nodemailer");
 const dotenv = require("dotenv").config();
 
 //test db
@@ -55,34 +54,14 @@ const createUser = (req, res) => {
       email,
       split,
     ])
-    .then(() => {
-      // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-        host: "secure233.servconfig.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-          user: "info@sidhn.com", // generated ethereal user
-          pass: "HLqiV7F(lglu", // generated ethereal password
-        },
-      });
-
-      // send mail with defined transport object
-      let info = transporter.sendMail({
-        from: '"Bill-Splitter" <info@sidhn.com>', // sender address
-        to: `${email}`, // list of receivers
-        subject: "Your lastest split", // Subject line
-        text: "Text", // plain text body
-        html: `<b>${split}</b>`, // html body
-      });
-
-      return res.json({
+    .then(() =>
+      res.json({
         message: "User Created",
         body: {
           user: { name, email, split },
         },
-      });
-    })
+      })
+    )
     .catch((err) => res.send(err));
 };
 
