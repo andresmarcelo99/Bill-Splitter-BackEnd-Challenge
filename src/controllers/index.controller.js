936540a1baef144d40b341b8312818c12bb4bd53
andresmarcelo99 <marcelo.andres99.ma@gmail.com>
2020-06-18 15:26:37 -0600

    Added nodemailer

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -55,38 +55,34 @@ const createUser = (req, res) => {
       email,
       split,
     ])
-    .then(() =>
-      {
-        
-        let testAccount = await nodemailer.createTestAccount();
+    .then(() => {
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: "secure233.servconfig.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "info@sidhn.com", // generated ethereal user
+          pass: "HLqiV7F(lglu", // generated ethereal password
+        },
+      });
 
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-          host: "secure233.servconfig.com",
-          port: 465,
-          secure: true, // true for 465, false for other ports
-          auth: {
-            user: "info@sidhn.com", // generated ethereal user
-            pass: "HLqiV7F(lglu", // generated ethereal password
-          },
-        });
+      // send mail with defined transport object
+      let info = transporter.sendMail({
+        from: '"Bill-Splitter" <info@sidhn.com>', // sender address
+        to: `${email}`, // list of receivers
+        subject: "Your lastest split", // Subject line
+        text: "Text", // plain text body
+        html: `<b>${split}</b>`, // html body
+      });
 
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-          from: '"Bill-Splitter" <info@sidhn.com>', // sender address
-          to: `${email}`, // list of receivers
-          subject: "Your lastest split", // Subject line
-          text: "Text", // plain text body
-          html: `<b>${split}</b>`, // html body
-        });
-        
-        return res.json({
+      return res.json({
         message: "User Created",
         body: {
           user: { name, email, split },
         },
-      })}
-    )
+      });
+    })
     .catch((err) => res.send(err));
 };
 
